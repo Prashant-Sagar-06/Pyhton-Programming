99572fc793902cab200a43e1b187db966e561c03
Prashant Sagar <142593105+Prashant-Sagar-06@users.noreply.github.com>
2024-03-13 22:27:36 +0530

    This code helps to find area of triangle using herons formula

--- Python_basic_with_functional_programming/areaof_tri_Herons.py
+++ Python_basic_with_functional_programming/areaof_tri_Herons.py
@@ -1,6 +1,11 @@
-a=float(input())
-b=float(input())
-c=float(input())
-semi=float(a+b+c)/2
-areaof_tri=float(semi*(semi-a)*(semi-b)*(semi-c))**0.5
-print(areaof_tri)
+#This programm find out area of triangle using herons formula
+import math
+
+a = float(input("Enter the length of side a: "))
+b = float(input("Enter the length of side b: "))
+c = float(input("Enter the length of side c: "))
+
+semi = (a + b + c) / 2
+area_of_tri = math.sqrt(semi * (semi - a) * (semi - b) * (semi - c))
+
+print("Area of the triangle:", area_of_tri)
