ff7bc126dc7758d8927cc01f0afb14d8636ef043
Prashant Sagar <142593105+Prashant-Sagar-06@users.noreply.github.com>
2024-03-13 22:39:32 +0530

    This programm tells the no of tiles in a given area

--- Python_basic_with_functional_programming/No.Of_Tiles.py
+++ Python_basic_with_functional_programming/No.Of_Tiles.py
@@ -1,8 +1,10 @@
-len_tile=int(input("enter"))
-breadth_tile=int(input("enter"))
-length_floor=int(input("enter"))
-breadth_floor=int(input("enter"))
-area_floor=length_floor*breadth_floor
-area_tile=len_tile*breadth_tile
-tiles=area_floor/area_tile
-print(tiles)
+len_tile = int(input("Enter the length of the tile: "))
+breadth_tile = int(input("Enter the breadth of the tile: "))
+length_floor = int(input("Enter the length of the floor: "))
+breadth_floor = int(input("Enter the breadth of the floor: "))
+
+area_floor = length_floor * breadth_floor
+area_tile = len_tile * breadth_tile
+
+tiles_needed = area_floor / area_tile
+print("Number of tiles needed:", tiles_needed)
