14adc8a08988bbf77ce6b9e5403ba5c44f77f1a3
Prashant Sagar <142593105+Prashant-Sagar-06@users.noreply.github.com>
2024-03-13 22:22:58 +0530

    Update even_odd_without_if_else.py and make code better

--- Python_basic_with_functional_programming/even_odd_without_if_else.py
+++ Python_basic_with_functional_programming/even_odd_without_if_else.py
@@ -1,6 +1,4 @@
-n=int(input())
-m=int(input())
-if m>n:
-    print(m-n)
-else:
-    print(n-m)
+#this programm tell that the given no is even or odd without using if and else
+number = int(input("Enter a number: "))
+result = ("Even", "Odd")[number % 2]
+print(f"The number {number} is {result}.")
