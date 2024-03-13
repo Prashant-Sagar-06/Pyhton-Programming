36b9c66fec43053900c42b19c7b497f1d9bc7561
Prashant Sagar <142593105+Prashant-Sagar-06@users.noreply.github.com>
2024-03-13 15:53:35 +0530

    A small descriptive code for find volume of sphere

--- Python_basic_with_functional_programming/volumesphere.py
+++ Python_basic_with_functional_programming/volumesphere.py
@@ -1,3 +1,4 @@
-r=int(input())
-volume=(4/3*(3.14*r**3))
-print(volume)
+#To find volume of spehere when radius is given or entered by user
+radii_Of_sphere=int(input())
+volume_of_sphere=(4/3*(3.14*radii_Of_sphere**3))
+print(volume_of_sphere)
