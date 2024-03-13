c3b7c51220790cfc9ba0b932d78d30a44788e749
Prashant Sagar <142593105+Prashant-Sagar-06@users.noreply.github.com>
2024-03-13 22:41:20 +0530

    This Programm tells the simple interest on given amount

--- Python_basic_with_functional_programming/Interest.py
+++ Python_basic_with_functional_programming/Interest.py
@@ -1,7 +1,15 @@
-p=int(input("enter principal"))
-r=int(input("enter rate"))
-t=int(input("enter time"))
-si=(p*r*t)/100
-amount=si+p
-print(si)
-print(amount)
+principal = int(input("Enter the principal amount: "))
+rate_of_interest = float(input("Enter the rate of interest (in percentage): "))
+time_in_years = int(input("Enter the time (in years): "))
+
+# Convert rate of interest from percentage to decimal
+rate_of_interest = rate_of_interest / 100
+
+# Calculate simple interest
+simple_interest = (principal * rate_of_interest * time_in_years)
+
+# Calculate total amount
+total_amount = simple_interest + principal
+
+print("Simple Interest:", simple_interest)
+print("Total Amount:", total_amount)
