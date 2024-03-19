c436184eaf6b0e5b36cd73b1691e9cdff7e23dcb
Prashant-Sagar-06 <sagar06032006chaudhary@gmail.com>
2024-03-19 10:17:57 +0530

    This programm simply returns the volume of sphere

--- Python_basic_with_functional_programming/volumesphere.py
+++ Python_basic_with_functional_programming/volumesphere.py
@@ -1,3 +1,7 @@
-r=int(input())
-volume=(4/3*(3.14*r**3))
-print(volume)
+radius = int(input("Enter the radius of the sphere: "))
+
+# Calculate the volume of the sphere
+sphere_volume = (4/3) * (3.14 * radius ** 3)
+
+# Display the volume of the sphere
+print("Volume of the sphere:", sphere_volume)
