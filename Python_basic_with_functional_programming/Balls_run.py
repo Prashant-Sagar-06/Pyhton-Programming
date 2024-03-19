b6e0f9e87b4c3790015a9e62c1a35cbc85ff22f2
Prashant-Sagar-06 <sagar06032006chaudhary@gmail.com>
2024-03-19 09:38:36 +0530

    This program display the no of runs in given balls

--- Python_basic_with_functional_programming/Balls_run.py
+++ Python_basic_with_functional_programming/Balls_run.py
@@ -1,5 +1,15 @@
-n=int(input("enter"))
-balls=n*6
-balls_6=(balls-(n-1))*6
-baals_3=(n-1)*3
-print(balls_6+baals_3)
+num_sets = int(input("Enter the number of sets: "))
+
+total_balls = num_sets * 6
+
+# Calculating the total number of balls considering the bonus for the last set
+balls_with_bonus = (total_balls - (num_sets - 1)) * 6
+
+# Calculating the total number of balls considering the extra balls from sets except the last one
+balls_without_bonus = (num_sets - 1) * 3
+
+# Calculating the final total number of balls
+final_total_balls = balls_with_bonus + balls_without_bonus
+
+# Displaying the final total number of balls
+print("The total number of balls is:", final_total_balls)
