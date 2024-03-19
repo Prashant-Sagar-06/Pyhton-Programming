7ffd5b5d9474bce2d52c73eebb61087194350713
Prashant-Sagar-06 <sagar06032006chaudhary@gmail.com>
2024-03-19 13:29:12 +0530

    This programm shows axis sum of array

---
+++ Numpy/sum_axis_num.py
@@ -0,0 +1,11 @@
+#For column sum
+import numpy as np
+arr=[[1,2,3,4],[4,5,6,7],[8,9,10,11]]
+out=np.sum(arr,0)
+print(out)
+
+#For row sum
+import numpy as np
+arr=[[1,2,3,4],[4,5,6,7],[8,9,10,11]]
+out=np.sum(arr,1)
+print(out)
