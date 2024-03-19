be4752e47ffaf875d0e4b9ef4f1140e5c692c01d
Prashant-Sagar-06 <sagar06032006chaudhary@gmail.com>
2024-03-19 09:52:49 +0530

    This programm tells the no is even or add without if -else

--- Python_basic_with_functional_programming/even_odd_without_if_else.py
+++ Python_basic_with_functional_programming/even_odd_without_if_else.py
@@ -1,6 +1,8 @@
-n=int(input())
-m=int(input())
-if m>n:
-    print(m-n)
-else:
-    print(n-m)
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+
+# Calculate the absolute difference between the two numbers
+absolute_difference = abs(num1 - num2)
+
+# Display the absolute difference
+print("Absolute difference between the two numbers is:", absolute_difference)
