9072811f678ba96f71089ad53a2711a5cb2bd185
Prashant-Sagar-06 <sagar06032006chaudhary@gmail.com>
2024-03-19 09:30:03 +0530

    This programm takes input of triangle base and height and return its area

--- Python_basic_with_functional_programming/area_of_triangle_.py
+++ Python_basic_with_functional_programming/area_of_triangle_.py
@@ -1,4 +1,8 @@
-a=int(input("enter base of triangle"))
-b=int(input("enter height of triangle"))
-c=1/2*a*b
-print(c)      
+base = int(input("Enter the base of the triangle: "))
+height = int(input("Enter the height of the triangle: "))
+
+# Calculating the area of the triangle
+area = 0.5 * base * height
+
+# Displaying the area of the triangle
+print("The area of the triangle is:", area)
