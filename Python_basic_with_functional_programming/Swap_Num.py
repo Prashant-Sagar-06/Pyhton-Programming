2530dd73ba7f891a7f28c1eda953c14cc6dade7f
Prashant-Sagar-06 <sagar06032006chaudhary@gmail.com>
2024-03-19 10:15:41 +0530

    This programm simply swap the  values of given numbers

--- Python_basic_with_functional_programming/Swap_Num.py
+++ Python_basic_with_functional_programming/Swap_Num.py
@@ -1,4 +1,10 @@
-a=int(input("enter"))
-b=int(input("enter"))
-a,b=b,a
-print(a,b)
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+
+# Swap the values of the variables
+first_number, second_number = second_number, first_number
+
+# Display the swapped values
+print("After swapping:")
+print("First number:", first_number)
+print("Second number:", second_number)
