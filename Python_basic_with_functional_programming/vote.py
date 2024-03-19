d9f4599badd517449c4366dfbbf314bdf217507b
Prashant-Sagar-06 <sagar06032006chaudhary@gmail.com>
2024-03-19 10:19:41 +0530

    This programm checks you are eligible to vote or not without using if and else

--- Python_basic_with_functional_programming/vote.py
+++ Python_basic_with_functional_programming/vote.py
@@ -1,4 +1,4 @@
-age = int(input("Enter your age: "))
-check = age >= 18
+Age = int(input("Enter your age: "))
+check = Age >= 18
 result = 'eligible' * check + 'not eligible' * (1 - check)
 print("Your age is",result)
