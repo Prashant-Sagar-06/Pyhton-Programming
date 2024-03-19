caa221ce37d58dbd6b2d5546e6e717f9cc5a8e1a
Prashant-Sagar-06 <sagar06032006chaudhary@gmail.com>
2024-03-19 13:14:41 +0530

    This programm shows numpy array on data type int8

--- Numpy/nparraysignunsign.py
+++ Numpy/nparraysignunsign.py
@@ -1,16 +1,19 @@
 #used all as separate code they are showing different condition
+#1
 import numpy as np
 ls=[-1,6,-2]
 data=np.array(ls,dtype="int8")
 print(data)
 
 
+#2
 import numpy as np
 ls=[-1,6,255]
 data=np.array(ls,dtype="int8")
 print(data)
 
 
+#3
 import numpy as np
 ls=[-1,6,256]
 data=np.array(ls,dtype="int8")
