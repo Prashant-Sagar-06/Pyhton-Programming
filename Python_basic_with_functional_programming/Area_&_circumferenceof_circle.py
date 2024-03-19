20a6cda705da4e6585245332ec451554bdbd807b
Prashant-Sagar-06 <sagar06032006chaudhary@gmail.com>
2024-03-19 09:26:21 +0530

    This programm take radius as input and give the area and circumference of circle

---
+++ Python_basic_with_functional_programming/Area_&_circumferenceof_circle.py
@@ -0,0 +1,9 @@
+radius = float(input("Enter the radius: "))
+
+# Calculating circumference and area
+circumference = 2 * 3.14 * radius
+area = 3.14 * radius ** 2
+
+# Displaying the results
+print("Circumference is =", circumference)
+print("Area is =", area)
--- Python_basic_with_functional_programming/area circumference circle.py
+++
@@ -1,5 +0,0 @@
-r=float(input("enter"))
-print("circumference is=" ,2*3.14*r)
-print("area is=", 3.14*r**2)
-
-
